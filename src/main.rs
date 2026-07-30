fae96a9b67df1765f7df48d92dc2e288f40bbf23
stan-2021131 <sergiocoromac@gmail.com>
2026-07-30 11:54:26 -0600

    feat: Inicialización de main render loop con minifb y uso básico del framebuffer para pruebas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use minifb::{Key, Window, WindowOptions};
+
+mod framebuffer;
+
+use crate::framebuffer::Framebuffer;
+
 fn main() {
-    println!("Hello, world!");
+    // Dimensiones de la pantalla
+    let window_width = 1280;
+    let window_height = 720;
+
+    // Dimensiones delFramebuffer
+    let framebuffer_width = 100;
+    let framebuffer_height = 100;
+
+    // Crear framebuffer
+    let mut framebuffer = Framebuffer::new(framebuffer_width, framebuffer_height);
+
+    // Crear ventana
+    let mut window = Window::new(
+        "Conway's Game of Life",
+        window_width,
+        window_height,
+        WindowOptions::default(),
+    )
+    .unwrap();
+    
+    // Configuración y creación de un punto usando el framebuffer
+    framebuffer.set_current_color(0xFFFFFF);
+    framebuffer.clear();
+
+    framebuffer.set_background_color(0x000000);
+    framebuffer.point(50, 50);
+    
+    // Bucle principal de la aplicación
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        window.update_with_buffer(&framebuffer.buffer, framebuffer.width, framebuffer.height)
+            .unwrap();
+    }
 }
