a4d0bb9382b950518275755fd1a8dc0d34ce1804
stan-2021131 <sergiocoromac@gmail.com>
2026-07-31 23:05:50 -0600

    feat(main): configurar ejecución del Juego de la Vida

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use minifb::{Key, Window, WindowOptions};
+use std::{thread, time::Duration};
 
 mod framebuffer;
+mod conways;
+mod desing;
 
 use crate::framebuffer::Framebuffer;
 
@@ -10,8 +13,8 @@ fn main() {
     let window_height = 720;
 
     // Dimensiones delFramebuffer
-    let framebuffer_width = 100;
-    let framebuffer_height = 100;
+    let framebuffer_width = 300;
+    let framebuffer_height = 200;
 
     // Crear framebuffer
     let mut framebuffer = Framebuffer::new(framebuffer_width, framebuffer_height);
@@ -25,16 +28,76 @@ fn main() {
     )
     .unwrap();
     
-    // Configuración y creación de un punto usando el framebuffer
-    framebuffer.set_current_color(0xFFFFFF);
+    // Configuración de los colores del framebuffer
+    framebuffer.set_background_color(0x06294c);
+    framebuffer.set_current_color(0xe5ec48);
+
     framebuffer.clear();
 
-    framebuffer.set_background_color(0x000000);
-    framebuffer.point(50, 50);
+    // Blinkers
+    framebuffer.blinker(120, 20);
+    framebuffer.blinker(110, 35);
+    framebuffer.blinker(250, 40);
+    framebuffer.blinker(180, 170);
+
+    // Boats
+    framebuffer.boat(45, 60);
+    framebuffer.boat(200, 20);
+    framebuffer.boat(260, 150);
+    framebuffer.boat(100, 130);
+
+    // Tubs
+    framebuffer.tub(20, 120);
+    framebuffer.tub(150, 80);
+    framebuffer.tub(280, 60);
+    framebuffer.tub(230, 170);
+
+    // Gliders
+    framebuffer.glider(30, 10);
+    framebuffer.glider(140, 25);
+    framebuffer.glider(260, 100);
+    framebuffer.glider(90, 170);
+
+    // HWSS
+    framebuffer.hwss(15, 150);
+    framebuffer.hwss(180, 50);
+    framebuffer.hwss(240, 120);
+
+    // Clocks
+    framebuffer.clock(60, 90);
+    framebuffer.clock(170, 150);
+    framebuffer.clock(250, 15);
+
+    // Bunnies
+    framebuffer.bunnies(110, 110);
+    framebuffer.bunnies(210, 160);
+    framebuffer.bunnies(150, 170);
+    framebuffer.bunnies(150, 50);
+
+    // Pulsars
+    framebuffer.pulsar(80, 60);
+    framebuffer.pulsar(210, 70);
+    framebuffer.pulsar(20, 170);
+
+    // Garden of Eden
+    framebuffer.goe(20, 20);
+    framebuffer.goe(180, 20);
+    framebuffer.goe(200, 100);
+
+    // Gosper Glider Gun
+    framebuffer.ggg(150, 100);
+    framebuffer.ggg(75, 120);
+    
     
     // Bucle principal de la aplicación
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        window.update_with_buffer(&framebuffer.buffer, framebuffer.width, framebuffer.height)
-            .unwrap();
+        framebuffer.update();
+        window.update_with_buffer(
+            &framebuffer.buffer,
+            framebuffer.width,
+            framebuffer.height,
+        ).unwrap();
+
+        thread::sleep(Duration::from_millis(200));
     }
 }
